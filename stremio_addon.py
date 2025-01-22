8841851554edacb0799f354f0a2be475e5643ea2
Zero0Q <elvissalihovic.es@gmail.com>
2025-01-22 18:04:12 +0800

    Update stremio_addon.py

--- stremio_addon.py
+++ stremio_addon.py
@@ -5,19 +5,16 @@ from catalog_response import catalog_response, get_available_saga_names
 
 app = Flask(__name__)
 
-
 def respond_with(data):
     response = jsonify(data)
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "*"
     return response
 
-
 @app.route("/manifest.json")
 def addon_manifest():
     manifest = set_manifest_sagas_from_available_sagas(MANIFEST)
     return respond_with(manifest)
-
 
 @app.route("/catalog/<media_type>/sagas/<saga_param>.json")
 def addon_catalog(media_type, saga_param):
@@ -26,11 +23,9 @@ def addon_catalog(media_type, saga_param):
     saga_name = saga_param.split("=")[-1]
     return respond_with(catalog_response(media_type, saga_name))
 
-
 def set_manifest_sagas_from_available_sagas(manifest):
     manifest["catalogs"][0]["extra"][0]["options"] = get_available_saga_names()
     return manifest
-
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
